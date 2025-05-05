9e34f3dae8402f836fe3a8949d1916b52eef3ddc
PAULO CEZAR PAIM DE JESUS <paulo_cp_jesus@estudante.sesisenai.org.br>
2025-05-05 16:50:18 -0300

    Insere Consulta da tabela.doc e Exercício CRUD.pdf com a resolução da lista de exercício, tambem atualiza o arquivo primeiro_BD.sql e Comandos iniciais de SQL.txt

--- primeiro_BD.sql
+++ primeiro_BD.sql
@@ -1,12 +1,1 @@
-CREATE DATABASE livraria;
-USE livraria;
-CREATE TABLE livros(
-id INTEGER PRIMARY KEY auto_increment,
-Titulo TEXT NOT NULL,
-Autor TEXT NOT NULL,
-AnoPublicacao INTEGER,
-Preco DECIMAL(10,2)
-);
-livraria
-SHOW TABLES;
-DESCRIBE livros;
+SELECT * FROM livros;
